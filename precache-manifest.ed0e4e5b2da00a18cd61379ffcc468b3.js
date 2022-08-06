69bd1dd11c973f70e4e0193f6b2a01ef5fd56cc7
Code Sandbox <hello@codesandbox.io>
2022-08-06 19:25:42 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.ed0e4e5b2da00a18cd61379ffcc468b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2723a06799f7592f5306705c59d4540",
+    "url": "https://lcaladoferreira.github.io/csb-0mpiws/index.html"
+  },
+  {
+    "revision": "8629210de3f11f589dac",
+    "url": "https://lcaladoferreira.github.io/csb-0mpiws/static/css/2.b94af48b.chunk.css"
+  },
+  {
+    "revision": "677c2c51c75de65d2965",
+    "url": "https://lcaladoferreira.github.io/csb-0mpiws/static/css/main.71484836.chunk.css"
+  },
+  {
+    "revision": "8629210de3f11f589dac",
+    "url": "https://lcaladoferreira.github.io/csb-0mpiws/static/js/2.5802f0e5.chunk.js"
+  },
+  {
+    "revision": "677c2c51c75de65d2965",
+    "url": "https://lcaladoferreira.github.io/csb-0mpiws/static/js/main.6d2a80d4.chunk.js"
+  },
+  {
+    "revision": "74d1c3d0cfbfb9d6fe27",
+    "url": "https://lcaladoferreira.github.io/csb-0mpiws/static/js/runtime~main.d1a7f263.js"
+  }
+]);
